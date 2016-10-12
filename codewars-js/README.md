b3b95ef15351d5922ee1509418c1bd85fd4bf81d
~ Mau ~ <mirvil7130@aol.com>
2016-10-12 16:42:54 -0400

    Worked through more problems

--- codewars-js/README.md
+++ codewars-js/README.md
@@ -1,1 +1,9 @@
 Codewars' JavaScript problems
+
+#1 Spin Words
+
+#2 Song Decoder
+
+#3 Square/Cube/Average/Sum/Even/Odd
+
+#4 Building Volume
--- codewars-js/main.js
+++ codewars-js/main.js
@@ -34,3 +34,130 @@ function songDecoder(song){
   return lyrics;
 }
 songDecoder(songLyrics);
+
+
+//MONDAY, 7/25/16
+
+//#3
+/*This kata is designed to test your ability to extend the functionality of built-in ruby classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+Explanation:
+square() must return a copy of the array, containing all values squared, the original array must not be changed
+cube() must return a copy of the array, containing all values cubed, the original array must not be changed
+average() must return the average of all array values, average() on an empty array must return NaN
+sum() must return the sum of all array values
+even() must return an array of all even numbers, the original array must not be changed
+odd() must return an array of all odd numbers, the original array must not be changed
+Examples:
+var numbers = [1, 2, 3, 4, 5];
+numbers.square(); // must return [1, 4, 9, 16, 25]
+numbers.cube(); // must return [1, 8, 27, 64, 125]
+numbers.average(); // must return 3
+numbers.sum(); // must return 15
+numbers.even(); // must return [2, 4]
+numbers.odd(); // must return [1, 3, 5]*/
+//will be using the existing methods but just for review, I'm defining each, map, reduce and filter
+function each(collection, callback){
+  if(Array.isArray(collection)){
+    for(var i = 0; i < collection.length; i++){
+      callback(collection[i]);
+    }
+  } else {
+    for (var key in collection){
+      callback(collection[key]);
+    }
+  }
+}
+function map(collection, transFunc){
+  var results = [];
+  if (!Array.isArray(collection)){
+    results = {};
+  }
+  each(collection, function(value){
+    results.push(value);
+  });
+  return results;
+}
+function reduce(collection, accumulator, startValue) {
+  var result = startValue;
+  each(collection, function(element) {
+    result = accumulator(result, element);
+  });
+  return result;
+}
+function filter(array, testFunc) {
+  var results = [];
+  each(array, function(value, index) {
+    if (testFunc(value, index) === true) {
+      results.push(value);
+    }
+  });
+  return results;
+}
+var numbers = [1, 2, 3, 4, 5];
+// SQUARE
+function squared(x){
+  return x * x;
+}
+Array.prototype.square = function(){
+  return this.map(squared);
+}
+// CUBE
+function cubed(x){
+  return x * x * x;
+}
+Array.prototype.cube = function(){
+  return this.map(cubed);
+}
+// SUM
+function summed(a, b){
+  return a + b;
+}
+Array.prototype.sum = function(){
+  return reduce(this, summed, 0);
+}
+// AVG
+Array.prototype.average = function() {
+  return this.sum() / this.length; 
+}
+
+// EVEN
+function evens(num){
+  if(num % 2 === 0){
+    return true;
+  }
+}
+Array.prototype.even = function(){
+  return this.filter(evens);
+}
+// ODD
+function odds(num){
+  if(!(num % 2 === 0)){
+    return true;
+  }
+}
+Array.prototype.odd = function(){
+  return this.filter(odds);
+}
+
+
+//WEDNESDAY, 10/12/16
+
+//#4
+/*Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+The parameter of the function findNb (find_nb, find-nb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+Examples:
+findNb(1071225) --> 45
+findNb(91716553919377) --> -1*/
+function findNb(m) { 
+  var vol = 0;
+  var n = 1;
+  while(vol < m){
+    vol = vol + (n * n * n);
+    if (vol === m) {
+      return n;
+    }
+    n++;
+  }
+  return (-1);
+}
